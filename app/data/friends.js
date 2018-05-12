38b3a5e7f83230955dc9c9a73f49dc85a059f108
thelongdinosaur <thelongdinosaur@gmail.com>
2018-05-12 19:56:25 -0400

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+
+var friends = [
+{
+    name: "Ahmed",
+    photo: "https://media.licdh.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+},
+{
+    name: "Ahmed2",
+    photo: "https://media.licdh.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "5",
+      "4",
+      "2",
+      "3"
+    ]
+},
+{
+    name: "Ahmed3",
+    photo: "https://media.licdh.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+      "5",
+      "1",
+      "5",
+      "4",
+      "5",
+      "3",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+},
+{
+    name: "Ahmed4",
+    photo: "https://media.licdh.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+},
+{
+    name: "Ahmed5",
+    photo: "https://media.licdh.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+      "1",
+      "5",
+      "3",
+      "2",
+      "1",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+},
+{
+    name: "Armando",
+    photo: "https://media.licdh.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+}
+]
+
+module.exports = friends;
